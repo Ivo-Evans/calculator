d5f612277ffed291103998ea5b1798040e25ab4c
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-23 18:50:09 +0000

    create variables and addToExpression function

--- interface.js
+++ interface.js
@@ -1,11 +1,31 @@
 'use strict'
 
+let expression = "";
+const numbers = new Array(10).fill().map((e, i) => String(i));
+const operators = ['+', '-', '*', '/', '\\', '**', '%']
 const calcButtons = document.getElementById('calculator');
 calcButtons.addEventListener('click', handleClick);
 
 function handleClick(e) {
   let event = e.target;
-  if (event.tagName !== "BUTTON" && event.tagName !== "INPUT") {
-    return;
+  if (event.tagName == "BUTTON") {
+    addToExpression(event);
+  } else if (event.tagName == "INPUT") {
+    handleInputTag(event);
+  } else {
+    return
+  }
+}
+
+function addToExpression(event) {
+  let choice = event.id;
+  if (numbers.includes(choice)) {
+    expression += choice;
+  } else if (operators.includes(choice)) {
+    expression += ` ${choice} `;
+  } else if (choice == '(') {
+    expression += `${choice} `;
+  } else if (choice == ')') {
+    expression += ` ${choice}`;
   }
 }
