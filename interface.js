7b4cfb021078da3fcab7fdb36883c5b2f185d020
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-24 03:11:37 +0000

    add enter key support to resolve equation

--- interface.js
+++ interface.js
@@ -7,7 +7,8 @@ const calcButtons = document.getElementById('calculator');
 const textBox = document.getElementById('formfield')
 
 textBox.addEventListener('input', manuallyWriteExpression); 
-document.addEventListener('keydown', () => {textBox.focus()}); // redesign: the top bar shouldn't be input, just <p>, and should be sensitive to keyups Anywhere on the page. 
+document.addEventListener('keydown', () => {textBox.focus()}); // redesign: the top bar shouldn't be input, just <p>, and should be sensitive to keyups Anywhere on the page.
+document.addEventListener('keyup', checkEnter) 
 calcButtons.addEventListener('click', handleClick);
 // TODO: the enter key should trigger resolve. Any other keydown should focus onto textBox so that keyup puts text into textBox
 // TODO: add (, ), ., and C buttons. C clears screen
@@ -17,6 +18,12 @@ function manuallyWriteExpression() {
   // this could just be a function expression really
   expression = textBox.value;
   expression = expression.replace(/\\/g, '/'); // the regex /// is read as a comment
+}
+
+function checkEnter(event) {
+  if (event.key == 'Enter') {
+    returnResult();
+  }
 }
 
 function handleClick(e) {
@@ -46,7 +53,11 @@ function addToExpression(event) {
 
 function handleInputTag(event) {
   if (event.id == 'equalsSign') {
-    expression = resolve(expression);
-    textBox.value = expression;
+    returnResult();
   }
+}
+
+function returnResult() {
+  expression = resolve(expression);
+  textBox.value = expression;
 }
