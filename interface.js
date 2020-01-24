85b7b982e6cd8f7e697d2d8d3fa6d096fad3151a
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-24 03:25:10 +0000

    symmetrical styling for calculator

--- interface.js
+++ interface.js
@@ -4,14 +4,13 @@ let expression = "";
 const numbers = new Array(10).fill().map((e, i) => String(i));
 const operators = ['+', '-', '*', '/', '\\', '**', '%']
 const calcButtons = document.getElementById('calculator');
-const textBox = document.getElementById('formfield')
+const textBox = document.getElementById('formfield');
 
 textBox.addEventListener('input', manuallyWriteExpression); 
-document.addEventListener('keydown', () => {textBox.focus()}); // redesign: the top bar shouldn't be input, just <p>, and should be sensitive to keyups Anywhere on the page.
-document.addEventListener('keyup', checkEnter) 
+document.addEventListener('keydown', () => {textBox.focus()}); // redesign: the top bar shouldn't be input, just <p>, and should be sensitive to keyups Anywhere on the page. This might also give you more control over styling. 
+document.addEventListener('keyup', checkEnter); 
 calcButtons.addEventListener('click', handleClick);
-// TODO: the enter key should trigger resolve. Any other keydown should focus onto textBox so that keyup puts text into textBox
-// TODO: add (, ), ., and C buttons. C clears screen
+// TODO: add ( ) . and C buttons. C clears screen
 
 
 function manuallyWriteExpression() {  
