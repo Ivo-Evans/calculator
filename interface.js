ef086cc04046fdbddacd52819f08275ead9f18a1
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-24 03:40:20 +0000

    add history scrolling with up and down arrow keys

--- interface.js
+++ interface.js
@@ -1,6 +1,8 @@
 'use strict'
 
 let expression = "";
+let history = [];
+let historyLocation = history.length;
 const numbers = new Array(10).fill().map((e, i) => String(i));
 const operators = ['+', '-', '*', '/', '\\', '**', '%']
 const calcButtons = document.getElementById('calculator');
@@ -8,7 +10,7 @@ const textBox = document.getElementById('formfield');
 
 textBox.addEventListener('input', manuallyWriteExpression); 
 document.addEventListener('keydown', () => {textBox.focus()}); // redesign: the top bar shouldn't be input, just <p>, and should be sensitive to keyups Anywhere on the page. This might also give you more control over styling. 
-document.addEventListener('keyup', checkEnter); 
+document.addEventListener('keyup', checkForSpecialKeys); 
 calcButtons.addEventListener('click', handleClick);
 // TODO: add ( ) . and C buttons. C clears screen
 
@@ -19,9 +21,17 @@ function manuallyWriteExpression() {
   expression = expression.replace(/\\/g, '/'); // the regex /// is read as a comment
 }
 
-function checkEnter(event) {
+function checkForSpecialKeys(event) {
   if (event.key == 'Enter') {
-    returnResult();
+    returnResult('new');
+  } else if (event.key == 'ArrowUp') {
+    if (historyLocation > 0) {historyLocation--};
+    expression = history[historyLocation]; // this functionality is used twice here and used in return result - a clear use-case for currying
+    textBox.value = expression;
+  } else if (event.key == 'ArrowDown') {
+    if (historyLocation < history.length - 1) {historyLocation++};
+    expression = history[historyLocation];
+    textBox.value = expression;
   }
 }
 
@@ -52,11 +62,12 @@ function addToExpression(event) {
 
 function handleInputTag(event) {
   if (event.id == 'equalsSign') {
-    returnResult();
+    returnResult('new');
   }
 }
 
-function returnResult() {
+function returnResult(flag) { // instead of flag you could use currying...
+  history.push(expression);
   expression = resolve(expression);
   textBox.value = expression;
 }
