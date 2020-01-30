c1772068c77e779e2406c437d0dcf99d4aa52ae7
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-30 03:17:15 +0000

    add support for 90% of minus number scenarios. equations like 50 + -(0-5), which are interpreted as 50+--5, currently throw an error. 50 + -5 is fine

--- iterative-solution.js
+++ iterative-solution.js
@@ -4,6 +4,7 @@ function resolve(string) {
   let equation = parse(string);
   if (equation) {
     equation = parentheses(equation);
+    equation = detectMinusNumber(equation);
 
     operations.forEach(operation => {
       equation = operation(equation);
@@ -27,9 +28,9 @@ function resolve(string) {
 
 function parse(string) {
   // console.log("parse called with " + string);
-  let rv = string.match(/\(.*\)|[0-9.]+|\*{2}|[-+*/%]/g);
+  return string.match(/\(.*\)|[0-9.]+|\*{2}|[-+*/%]/g);
   // console.log("parse returns " + rv);
-  return rv;
+  // return rv;
   /* 
 so what's going on here? Parse scans the input string and creates an array with four kinds of values, separated and prioritised by alternators:
 1) First it looks for any sub-expressions within brackets. It works for nested brackets (i.e. matches according to the outside layer of brackets only) - I'm not sure why, maybe because regex is 'greedy'
@@ -41,14 +42,44 @@ So if you feed it 5+-92*9**2*(9-2) you will get ["5", "+", "-", "92", "*", "9", 
 */
 }
 
-function parentheses(equation) {
+
+function detectMinusNumber(equation) {
+  for(let i = 0; i < equation.length; i++) {
+    if (equation[i] === '-') {
+      if (operators.includes(equation[i - 1])) {
+        equation[i] += equation[i + 1];
+        equation[i + 1] = null;
+      }
+    }
+  }
+  return equation.filter((element => element !== null));
+}
+
+function parentheses(equation) { // TODO: JavaScript doesn't know how to read double minus numbers as positive numbers, so you will have to do that for it. --5 returns an error, but it is a prima facie valid mathematical operation (as in, e.g. 500+--5, as is returned by 500 + -(0-5)).
   // console.log("parentheses called with " + equation);
   // console.log(equation)
   equation = equation.map(element => {
-    return element[0] == "("
-      ? resolve(element.slice(1, element.length - 1))
-      : element;
-  });
+    if (element[0] === "(") {
+      return resolve(element.slice(1, element.length -1));
+    } else if (element[1] === "(") {
+      return Number("-" - resolve(element.slice(2, element.length - 1)));
+    } else {
+      return element;
+    }
+  })
   // console.log("parentheses returns " + equation);
   return equation;
 }
+
+
+// function parentheses(equation) {
+//   // console.log("parentheses called with " + equation);
+//   // console.log(equation)
+//   equation = equation.map(element => {
+//     return element[0] == "("
+//       ? resolve(element.slice(1, element.length - 1))
+//       : element;
+//   });
+//   // console.log("parentheses returns " + equation);
+//   return equation;
+// }
