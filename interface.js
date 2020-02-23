9f3beca60b85f0a487aed1ff47c3687e0dfca47b
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-02-23 13:10:53 +0000

    add swipe navigation

--- interface.js
+++ interface.js
@@ -3,16 +3,22 @@
 let expression = "";
 let history = [];
 let historyLocation = history.length;
-const numbers = new Array(10).fill().map((e, i) => String(i));
-const operators = ["+", "-", "*", "/", "\\", "**", "%"];
-const calcButtons = document.getElementById("calculator");
+let swipeStart;
+
+const calculator = document.getElementById("calculator");
 const textBox = document.getElementById("formfield");
 const historyBox = document.getElementById("history")
+
+const validCharacters = Array.from(calculator.querySelectorAll('button')).map(b => b.innerText);
+const numbers = new Array(10).fill().map((e, i) => String(i));
+const operators = ["+", "-", "*", "/", "\\", "**", "%"];
 
 textBox.addEventListener("input", manuallyWriteExpression);
 document.addEventListener("keydown", () => {textBox.focus()});
 document.addEventListener("keyup", checkForSpecialKeys);
-calcButtons.addEventListener("click", handleClick);
+calculator.addEventListener("click", handleClick);
+calculator.addEventListener("touchstart", logY);
+calculator.addEventListener("touchend", handleSwipe)
 
 function manuallyWriteExpression() {
   // this could just be a function expression really
@@ -34,7 +40,7 @@ function checkForSpecialKeys(event) {
 
 function traverseHistory(direction) {
  let end = history.length - 1;
-  if ((historyLocation > 0 && direction < 0) || (historyLocation < end && direction > 0)) {
+  if (historyLocation > 0 && direction < 0 || historyLocation < end && direction > 0) {
     historyLocation += direction;
     jumpToHistory();
     updateHistoryBar();
@@ -89,6 +95,21 @@ function updateHistoryBar() {
   }
 }
 
+function logY(e) {
+  swipeStart = e.touches[0].pageY;
+}
 
-// TODO: implement swipe navigation for history browsing
+function handleSwipe(e) {
+  let swipe = e.changedTouches[0].pageY - swipeStart;
+  let minimumSwipe = 10;
+
+  if (swipe > minimumSwipe) {
+    traverseHistory(1)
+  } else if (swipe < -minimumSwipe) {
+    traverseHistory(-1)
+  }
+
+  swipeStart = null;
+}
+
 // TDOD: implement opacity change on keydown and removal on keyup, as well as disallowal of bad characters. In other words, rework the keyboard input system. 
